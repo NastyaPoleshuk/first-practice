3c518a5e85b40af36272997e7d76fa828acf6692
anastasiya.poleshchuk <nastyapoleshuk28@gmail.com>
2026-06-22 15:30:06 +0800

    Добавлена проверка на правильный порядок улиц и увеличен объём хранимых данных

--- task3/task3.cpp
+++ task3/task3.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include<vector>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
 
-void Snow(std::vector<int>& streets, int i, int x) {
+void Snow(std::vector<int64_t>& streets, int i, int64_t x) {
   streets[i - 1] += x;
 }
-int IsSnow(std::vector<int>& streets, int u, int r) {
-  int count = 0;
+int64_t IsSnow(std::vector<int64_t>& streets, int u, int r) {
+  int64_t count = 0;
+  if (u > r) {
+    std::swap(u, r);
+  }
   while (u <= r) {
     count += streets[u - 1];
     ++u;
@@ -16,18 +21,20 @@ int main() {
   int n = 0;
   int k = 0;
   std::cin >> n >> k;
-  std::vector<int> streets(n, 0);
-  int num = 0;
-  int i = 0;
-  int x = 0;
+  std::vector<int64_t> streets(n, 0);
   for (int q = 0; q < k; ++q) {
+    int num = 0;
     std::cin >> num;
     if (num == 1) {
+      int i = 0;
+      int64_t x = 0;
       std::cin >> i >> x;
       Snow(streets, i, x);
     } else if (num == 2) {
-      std::cin >> i >> x;
-      std::cout << IsSnow(streets, i, x) << "\n";
+      int i = 0;
+      int u = 0;
+      std::cin >> i >> u;
+      std::cout << IsSnow(streets, i, u) << "\n";
     }
   }
   return 0;
